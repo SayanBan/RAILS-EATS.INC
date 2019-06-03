31ff38b480efe72ecce1310c977709e7e103e84b
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-06-03 12:16:36 +0530

    Create users_controller.rb

---
+++ app/controller/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end 
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_url
+      else
+        redirect_to signup_url
+    end
+  end 
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+
+end
